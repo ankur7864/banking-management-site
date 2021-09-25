92fd7fa38a84bab501466f0dea863f7324e5ff0a
ankur7864 <57158482+ankur7864@users.noreply.github.com>
2021-09-25 11:51:17 +0530

    Update all.js

--- models/all.js
+++ models/all.js
@@ -1,12 +1,6 @@
 const mongoose = require("mongoose");
 
-// mongoose.connect("mongodb://localhost:27017/banking", { useUnifiedTopology: true, useNewUrlParser: true })
-//     .then(() => {
-//         console.log("connected");
-//     })
-//     .catch((err) => {
-//         console.log("error", err);
-//     })
+
 
 const databSchema = new mongoose.Schema({
     Date: Date,
